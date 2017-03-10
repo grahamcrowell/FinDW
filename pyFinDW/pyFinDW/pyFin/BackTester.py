a7406ee804e853672d7852dea0663ebeee7b795f
Crowell <GCrowell@vch.ca>
2017-03-09 19:32:10 -0800

    some comments.

--- pyFinDW/pyFinDW/pyFin/BackTester.py
+++ pyFinDW/pyFinDW/pyFin/BackTester.py
@@ -9,13 +9,13 @@ class StockUniverse(object):
 	def get_available_stocks(self, date):
 		"""returns collection of FinTimeSeries objects"""
 		raise NotImplemented
-	
 
 
 class Recommendation(Enum):
 	"""simple flag indicating a BUY or SELL Recommendation"""
 	SELL = -1
 	BUY = 1
+
 
 class Strategy(object):
 	def __init__(self):
@@ -26,7 +26,9 @@ class Strategy(object):
 		return Recommendation.BUY
 		raise NotImplemented
 
+
 class BackTester(object):
+	"""main back testing engine"""
 	def __init__(self, strategy, stock_universe):
 		self.__start_date = start_date
 		self.__end_date = end_date
@@ -34,4 +36,8 @@ class BackTester(object):
 		self.__portfolio_total_weighting = 100.0
 		self.__strategy = strategy
 	def begin(self):
+		pass
+	
+	def rate_of_return(self):
+		"""returns the profitability of strategy"""
 		pass
